c4b7ff13ef443c548392bacce00c86a79c4465be
Ofir Shtrull <ofirs@tikalk.com>
2024-11-04 10:49:47 -0500

    add ganatlytcs to linkerd

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript for interactivity will go here
+document.addEventListener('DOMContentLoaded', function () {
+    var linkedinLink = document.getElementById('linkedin-link');
+    if (linkedinLink) {
+        linkedinLink.addEventListener('click', function (event) {
+            // Send event to Google Analytics
+            gtag('event', 'click', {
+                'event_category': 'Outbound Link',
+                'event_label': 'LinkedIn Profile',
+                'event_action': 'click',
+                'event_callback': function () {
+                    // Optional: Navigate to the link after event is sent
+                    window.open(linkedinLink.href, '_blank');
+                }
+            });
+        });
+    }
+});
